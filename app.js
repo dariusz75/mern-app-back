f7433cfb51a613a54e8bd9f1718a56c207554303
Dariusz Franczak <darius32@op.pl>
2021-02-26 19:17:05 +0000

    unsupported route error handling

--- app.js
+++ app.js
@@ -2,12 +2,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const placesRoutes = require('./routes/places-routes');
+const HttpError = require('./models/http-error');
 
 const app = express();
 
 app.use(bodyParser.json());
 
 app.use('/api/places', placesRoutes);
+
+app.use((request, response, next) => {
+  const error = new HttpError('Could not find this route.', 404);
+  next(error);
+});
 
 app.use((error, request, response, next) => {
   if (response.headerSent) {
